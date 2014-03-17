10f7dade1bef54776c96bf7ece25a971be030f23
Dibyendu Mitra Roy <dibs439@gmail.com>
2014-03-18 01:01:55 +0530

    First commit to server

---
+++ services/config.php
@@ -0,0 +1,14 @@
+<?php
+	
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = 'system';
+$dbname = 'employee';
+
+/*$dbhost = '166.62.8.44';
+$dbuser = 'prologicdirector';
+$dbpass = 'Pro@L0g1c';
+$dbname = 'prologicdirector';*/
+
+
+?>
